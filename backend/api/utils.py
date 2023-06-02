4e58918d6a315a37265cc60da4fc092403c7b103
Anton Ign <i_a_v_86@mail.ru>
2023-06-02 16:54:53 +0300

    fixes

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,9 +1,33 @@
 from datetime import datetime
 
 from django.shortcuts import HttpResponse
+from django.shortcuts import get_object_or_404
+from recipes.models import Recipe
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import RecipeShortSerializer
 
+def favorite_shopping_cart(request, model, **kwargs):
+    recipe = get_object_or_404(Recipe, id=kwargs['pk'])
 
-def ingredients_export(self, request, ingredients):
+    if request.method == 'POST':
+        serializer = RecipeShortSerializer(recipe, data=request.data,
+                                      context={"request": request})
+        serializer.is_valid(raise_exception=True)
+        if not model.objects.filter(user=request.user,
+                                    recipe=recipe).exists():
+            model.objects.create(user=request.user, recipe=recipe)
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
+        return Response({'errors': 'Рецепт уже добавлен.'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    get_object_or_404(model, user=request.user,
+                      recipe=recipe).delete()
+    return Response({'detail': 'Рецепт успешно удален.'},
+                    status=status.HTTP_204_NO_CONTENT)
+
+def ingredients_export(request, ingredients):
     user = request.user
     filename = f'{user.username}_shopping_list.txt'
     today = datetime.today()
