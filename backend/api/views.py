6b909b47649e7f5354b4570c53e4116c7ba70c12
Anton Ign <i_a_v_86@mail.ru>
2023-06-02 14:17:26 +0300

    fixes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,8 @@
 from django.db.models import Sum
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from recipes.models import (Favourite, Ingredient, IngredientInRecipe, Recipe,
+                            ShoppingCart, Tag)
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
@@ -8,18 +10,12 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-from recipes.models import (
-    Favourite, Ingredient, IngredientInRecipe,
-    Recipe, ShoppingCart, Tag
-)
 from .filters import IngredientFilter, RecipeFilter
 from .paginators import CustomPagination
 from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
-from .serializers import (
-    IngredientSerializer, RecipeReadSerializer,
-    RecipeShortSerializer, RecipeWriteSerializer,
-    TagSerializer
-)
+from .serializers import (IngredientSerializer, RecipeReadSerializer,
+                          RecipeShortSerializer, RecipeWriteSerializer,
+                          TagSerializer)
 from .utils import ingredients_export
 
 
