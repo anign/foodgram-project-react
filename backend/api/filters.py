a39a2ce69cdd0f40c88e7b37af3d8ea61d901ee9
Anton Ign <i_a_v_86@mail.ru>
2023-06-03 12:15:46 +0300

    another fixes

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -15,29 +15,26 @@ class IngredientFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        to_field_name='slug',
-        queryset=Tag.objects.all(),
-    )
-
-    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    tags = filters.ModelMultipleChoiceFilter(field_name='tags__slug',
+                                             to_field_name='slug',
+                                             queryset=Tag.objects.all())
+    is_favorited = filters.BooleanFilter(
+        method='is_favorited_filter')
     is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_is_in_shopping_cart'
-    )
+        method='is_in_shopping_cart_filter')
 
     class Meta:
         model = Recipe
         fields = ('tags', 'author',)
 
-    def filter_is_favorited(self, queryset, value):
+    def is_favorited_filter(self, queryset, name, value):
         user = self.request.user
-        if value and not user.is_anonymous:
-            return queryset.filter(favorites__user=user)
+        if value and user.is_authenticated:
+            return queryset.filter(favorite_recipe__user=user)
         return queryset
 
-    def filter_is_in_shopping_cart(self, queryset, value):
+    def is_in_shopping_cart_filter(self, queryset, name, value):
         user = self.request.user
-        if value and not user.is_anonymous:
-            return queryset.filter(shopping_cart__user=user)
+        if value and user.is_authenticated:
+            return queryset.filter(shopping_recipe__user=user)
         return queryset
