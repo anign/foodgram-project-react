37230bbeb9b17a1f47ffd990ae004cf3161563bb
Anton Ign <i_a_v_86@mail.ru>
2023-06-03 15:50:16 +0300

    фÐtags many=False

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -151,7 +151,7 @@ class IngredientInRecipeWriteSerializer(ModelSerializer):
 class RecipeWriteSerializer(ModelSerializer):
     tags = PrimaryKeyRelatedField(
         queryset=Tag.objects.all(),
-        many=True
+        many=False
     )
     author = CustomUserSerializer(read_only=True)
     ingredients = IngredientInRecipeWriteSerializer(many=True)
